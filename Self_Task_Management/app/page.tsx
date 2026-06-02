e6ba6887395f61986e67cc9c31015d2d94344781
NguyenPhan161206 <phbn303030@gmail.com>
2026-06-02 13:18:18 +0700

    defaul

---
+++ Self_Task_Management/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div>
+          <h1 className="font-medium">Project ready!</h1>
+          <p>You may now add components and start building.</p>
+          <button className="mt-2 rounded-md border px-3 py-1 text-sm">Button</button>
+        </div>
+        <div className="font-mono text-xs text-muted-foreground">
+          (Press <kbd>d</kbd> to toggle dark mode)
+        </div>
+      </div>
+    </div>
+  )
+}
